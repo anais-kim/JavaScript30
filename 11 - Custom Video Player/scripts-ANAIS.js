d00c0d7fd898583da73b65e198c8d47c5f2cbfcc
anais-kim <itschstyle@gmail.com>
2019-10-05 17:43:07 +0900

    [Improvement] Add fullscreen mode toggle

--- 11 - Custom Video Player/scripts-ANAIS.js
+++ 11 - Custom Video Player/scripts-ANAIS.js
@@ -1,10 +1,11 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('video');
-const toggle = player.querySelector('.toggle');
+const playButton = player.querySelector('.toggle');
 const sliders = player.querySelectorAll('.player__slider');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
+const fullscreenButton = player.querySelector('.fullscreen');
 
 async function playVideo() {
     try {
@@ -19,8 +20,8 @@ function togglePlay() {
     else video.pause();
 }
 
-function updateButton() {
-    toggle.textContent = (video.paused)? '►' : '❚ ❚';
+function updatePlayButton() {
+    playButton.textContent = (video.paused)? '►' : '❚ ❚';
 }
 
 function handleRangeUpdate() {
@@ -40,12 +41,27 @@ function updateProgress(e) {
     video.currentTime = e.offsetX / progress.offsetWidth * video.duration;
 }
 
+function handleFullscreen() {
+    if (document.fullscreenElement === player) document.exitFullscreen();
+    else player.requestFullscreen();
+}
+
+function updateFullscreenButton() {
+    fullscreenButton.innerHTML = document.fullscreenElement === player? 
+        '<i class="icon-resize-small"></i>' : 
+        '<i class="icon-fullscreen"></i>';
+}
+
+function updateVideoToFullscreen() {
+    player.style.border = document.fullscreenElement === player? 0 : '';
+}
+
 video.addEventListener('click', togglePlay);
-video.addEventListener('play', updateButton);
-video.addEventListener('pause', updateButton);
+video.addEventListener('play', updatePlayButton);
+video.addEventListener('pause', updatePlayButton);
 video.addEventListener('timeupdate', fillProgressBar);
 
-toggle.addEventListener('click', togglePlay);
+playButton.addEventListener('click', togglePlay);
 
 sliders.forEach(slider => {
     slider.addEventListener('change', handleRangeUpdate);
@@ -59,3 +75,7 @@ progress.addEventListener('click', updateProgress);
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
 progress.addEventListener('mousemove', (e) => mousedown && updateProgress(e));
+
+fullscreenButton.addEventListener('click', handleFullscreen);
+player.addEventListener('fullscreenchange', updateFullscreenButton);
+player.addEventListener('fullscreenchange', updateVideoToFullscreen);
