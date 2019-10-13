f93eb58bbd04897efb95f5f556c4aba690bbb24e
anais-kim <itschstyle@gmail.com>
2019-10-13 11:31:38 +0900

    [Refactor] improve filters

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -40,7 +40,7 @@ function takePhoto() {
     strip.insertBefore(link, strip.firstChild);
 }
 
-function redFilter(pixels) {
+function redEffect(pixels) {
     for (let i = 0; i <= pixels.data.length; i+=4) {
         pixels.data[i+1] = 0;
         pixels.data[i+2] = 0;
@@ -49,28 +49,30 @@ function redFilter(pixels) {
 
 function rgbSplit(pixels) {
     for (let i = 0; i <= pixels.data.length; i+=4) {
-        pixels.data[i] = pixels.data[i+100];
-        pixels.data[i+1] = pixels.data[i+1+400];
-        pixels.data[i+2] = pixels.data[i+2+700];
+        pixels.data[i-100] = pixels.data[i];
+        pixels.data[i+200] = pixels.data[i+1];
+        pixels.data[i-202] = pixels.data[i+2];
     }
 }
 
 function greenScreen({data: rgb}) {
-    const ranges = [...greenControls.querySelectorAll('input[type=range]')];
-    const {rmin, rmax, gmin, gmax, bmin, bmax} = ranges.reduce((totalRange, range) => {
-        if (!totalRange[range.name]) totalRange[range.name] = range.value;
-        return totalRange;
-    }, {});
+    const range = {};
+    greenControls.querySelectorAll('input[type=range]').forEach(input => {
+        range[input.name] = input.value;
+    });
 
     for (let i = 0; i < rgb.length; i+=4) {
         const r = rgb[i];
         const g = rgb[i+1];
         const b = rgb[i+2];
 
-        if (r >= rmin && r <= rmax && g >= gmin && g <= gmax && b >= bmin && b <= bmax) {
-            rgb[i] = 255;
-            rgb[i+1] = 255;
-            rgb[i+2] = 255;
+        if (r >= range.rmin 
+         && r <= range.rmax 
+         && g >= range.gmin 
+         && g <= range.gmax 
+         && b >= range.bmin 
+         && b <= range.bmax) {
+            rgb[i+3] = 0;
         }
     }
 }
@@ -87,15 +89,13 @@ function screenVideo() {
 
         switch (effect) {
             case 'red':
-                redFilter(pixels);
+                redEffect(pixels);
                 break;
             case 'rgb':
                 rgbSplit(pixels);
                 break;
             case 'green':
                 greenScreen(pixels);
-                break;
-            case 'none':
                 break;
         }
         ctx.putImageData(pixels, 0, 0);
